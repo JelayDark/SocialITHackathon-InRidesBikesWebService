d793e455f0836525d30be17966d1b8930d430637
eugeniy_T <etulyakov@datas-tech.com>
2018-06-18 04:50:23 +0300

    added components for room

---
+++ inrides-wbpck/src/components/Room/ChatWindow.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "chat-window"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ inrides-wbpck/src/components/Room/RoomMap.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "room-map"
+    }
+</script>
+
+<style scoped>
+
+</style>
